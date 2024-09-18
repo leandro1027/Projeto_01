b5783de871b7db22d646df622403514ceb56562c
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-09-18 20:00:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en, pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,30 +9,31 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>WEB 2</title>
 </head>
+
 <body>
 
     <!--header-->
-    <header> 
-    <div class="center">
-        <div class="logo"></div>
-        <nav class="desktop">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
-        
-        <nav class="mobile">
-        <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
-    </div>
+    <header>
+        <div class="center">
+            <div class="logo"></div>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+
+            <nav class="mobile">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
     <!--header-->
 
@@ -52,7 +54,7 @@
         <div class="center">
             <div class="w50">
                 <h2>Leandro Balaban</h2>
-                <p>Lorem ipsum, dolor sit amet 
+                <p>Lorem ipsum, dolor sit amet
                     consectetur adipisicing elit. Dolor rerum assumendaepellat,
                     exercitationem .</p>
             </div>
@@ -70,25 +72,25 @@
             <div class="box-especialidades">
                 <h3>icone 1</h3>
                 <h3>HTML 5</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur 
+                <p>Lorem ipsum dolor, sit amet consectetur
                     adipisicing elit. Iste dolorum excepturi itaque veritatis
-                    quam magnam, facilis eligendi incidunt consectetur 
+                    quam magnam, facilis eligendi incidunt consectetur
                     voluptas praesentium aliquid rerum fugiat</p>
             </div>
             <div class="box-especialidades">
                 <h3>icone 2</h3>
                 <h3>CSS 3</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur 
+                <p>Lorem ipsum dolor, sit amet consectetur
                     adipisicing elit. Iste dolorum excepturi itaque veritatis
-                    quam magnam, facilis eligendi incidunt consectetur 
+                    quam magnam, facilis eligendi incidunt consectetur
                     voluptas praesentium aliquid rerum fugiat</p>
             </div>
             <div class="box-especialidades">
                 <h3>icone 3</h3>
                 <h3>JS</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur 
+                <p>Lorem ipsum dolor, sit amet consectetur
                     adipisicing elit. Iste dolorum excepturi itaque veritatis
-                    quam magnam, facilis eligendi incidunt consectetur 
+                    quam magnam, facilis eligendi incidunt consectetur
                     voluptas praesentium aliquid rerum fugiat</p>
             </div>
         </div>
@@ -103,7 +105,7 @@
                 <div class="depoimentos-single">
                     <p class="depoimentos-descricao">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Assumenda, totam repudiandae? Officiis id nemo iste ipsa 
+                        Assumenda, totam repudiandae? Officiis id nemo iste ipsa
                         perferendis quas totam natus voluptatem
                         ,quisquam, quidem illo velit vel illum qui accusantium reiciendis?
                     </p>
@@ -111,15 +113,49 @@
                 </div>
                 <div class="depoimeto-single">
                     <p class="depoimento-descricao">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate
                         saepe consequuntur voluptatibus quae placeat, nostrum sunt illo numquam,
                         aliquid dolore, quas ullam recusandae? Iste eum rem qui laboriosam minima.
                     </p>
+                    <p class="nome-autor">Lorem ipsum</p>
                 </div>
             </div>
+            <div class="depoimento-single">
+                <p class="depoimento-single">
+                <p class="depoimento-descricao"> Lorem ipsum dolor sit,
+                    amet consectetur adipisicing elit. Voluptatibus cum ratione
+                    perferendis nobis sed illo molestiae magnam corporis mollitia,
+                    consequuntur molestiaspariatur commodi sint tempora praesentium!
+                    rem magnam!</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+            </div>
+        </div>
+        <div class="w50">
+            <h2 class="title">
+                <div class="servicos">
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem 
+                            animi aut, eius, ullam quo illum veniam excepturi nostrum a cumque facilis
+                             deleniti laudantium ratione dicta? Enim hic aspernatur praesentium tenetur.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem 
+                            animi aut, eius, ullam quo illum veniam excepturi nostrum a cumque facilis
+                             deleniti laudantium ratione dicta? Enim hic aspernatur praesentium tenetur.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem 
+                            animi aut, eius, ullam quo illum veniam excepturi nostrum a cumque facilis
+                             deleniti laudantium ratione dicta? Enim hic aspernatur praesentium tenetur.
+                        </li>
+                    </ul>
+                </div>
+            </h2>
         </div>
     </section>
-    <!--Extras-->    
-    
+    <!--Extras-->
+
 </body>
+
 </html>
