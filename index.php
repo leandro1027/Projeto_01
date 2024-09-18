2ce42896742f5058d84e58da663d0f66658bc809
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-09-18 19:52:09 -0300

    .

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en, pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição">
+    <meta name="keywords" content="Palavra-chave 1, 2, 3">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>WEB 2</title>
+</head>
+<body>
+
+    <!--header-->
+    <header> 
+    <div class="center">
+        <div class="logo"></div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        
+        <nav class="mobile">
+        <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </div>
+    </header>
+    <!--header-->
+
+    <!--Banner principal-->
+    <section class="banner-principal">
+        <div class="center">
+            <form action="">
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" id="email" required>
+                <input type="submit" name="enviar" value="Enviar">
+            </form>
+        </div>
+    </section>
+    <!--Banner principal-->
+
+    <!--Descrição autor-->
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50">
+                <h2>Leandro Balaban</h2>
+                <p>Lorem ipsum, dolor sit amet 
+                    consectetur adipisicing elit. Dolor rerum assumendaepellat,
+                    exercitationem .</p>
+            </div>
+            <div class="w50">
+                <img src="assets/img/local-trabalho.png" alt="Local de trabalho">
+            </div>
+        </div>
+    </section>
+    <!--Descrição autor-->
+
+    <!--Especialidades-->
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box-especialidades">
+                <h3>icone 1</h3>
+                <h3>HTML 5</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur 
+                    adipisicing elit. Iste dolorum excepturi itaque veritatis
+                    quam magnam, facilis eligendi incidunt consectetur 
+                    voluptas praesentium aliquid rerum fugiat</p>
+            </div>
+            <div class="box-especialidades">
+                <h3>icone 2</h3>
+                <h3>CSS 3</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur 
+                    adipisicing elit. Iste dolorum excepturi itaque veritatis
+                    quam magnam, facilis eligendi incidunt consectetur 
+                    voluptas praesentium aliquid rerum fugiat</p>
+            </div>
+            <div class="box-especialidades">
+                <h3>icone 3</h3>
+                <h3>JS</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur 
+                    adipisicing elit. Iste dolorum excepturi itaque veritatis
+                    quam magnam, facilis eligendi incidunt consectetur 
+                    voluptas praesentium aliquid rerum fugiat</p>
+            </div>
+        </div>
+    </section>
+    <!--Especialidades-->
+
+    <!--Extras-->
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimentos-single">
+                    <p class="depoimentos-descricao">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Assumenda, totam repudiandae? Officiis id nemo iste ipsa 
+                        perferendis quas totam natus voluptatem
+                        ,quisquam, quidem illo velit vel illum qui accusantium reiciendis?
+                    </p>
+                    <p class="nome-autor">Lorem ipsum</p>
+                </div>
+                <div class="depoimeto-single">
+                    <p class="depoimento-descricao">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate 
+                        saepe consequuntur voluptatibus quae placeat, nostrum sunt illo numquam,
+                        aliquid dolore, quas ullam recusandae? Iste eum rem qui laboriosam minima.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Extras-->    
+    
+</body>
+</html>
