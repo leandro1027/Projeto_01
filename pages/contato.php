abfc8f10900ff4912813f067c8dd2ff641b55dd8
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-10-09 22:15:44 -0300

    .

--- pages/contato.php
+++ pages/contato.php
@@ -1,1 +1,20 @@
-<h1>Págoina de contato</h1>
+<div class="contato-container">
+    <div class="center">
+        <form action="">
+            <input type="text" naome="nome" id="" placeholder="Nome: ">
+            <input type="text" name="email" id="" placeholder="E-mail: ">
+            <input type="text" name="telefone" id="" placeholder="Telefone: ">
+            <textarea name="mensagem" id="" placeholder="Digite sua mensagem"></textarea>
+            <input type="submit" name="btnEnviar" value="Enviar">
+        </form>
+    </div>
+</div>
+
+<!--manipular no css-->
+<div class="map">
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.025795598358!2d-51.05138442370912!3d-26.228351364959494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e6602a2f792417%3A0x260a3f120bc90789!2sIFPR%20-%20Campus%20Uni%C3%A3o%20da%20Vit%C3%B3ria!5e0!3m2!1spt-BR!2sbr!4v1728521645810!5m2!1spt-BR!2sbr"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+</div>
