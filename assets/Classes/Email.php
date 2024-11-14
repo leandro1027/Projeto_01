56e14d0d592fd7858147897ae940cbc5f3dcd9c6
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-13 21:23:44 -0300

    .

---
+++ assets/Classes/Email.php
@@ -0,0 +1,12 @@
+<?php
+class Email{
+
+    function __construct(){
+        echo 'testando';
+
+
+    }
+    
+
+}
+?>
--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
+ //definir o dominio
     define('INCLUDE_PATH', 'http://localhost/Projeto_01/');
 ?>
